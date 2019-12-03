ab6e71fa731b70ecac59ae90562eb59e889facb5
ElizathNatasha <mulekaliz@gmail.com>
2019-12-03 17:43:54 +0300

    added if statements

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,15 @@
 VarDate = prompt("please enter your age");
 
-const age = 18; 
+let age = 18; 
+
+if(age < 18){
+    console.log("minor");
+}
+
+if(age >= 18 && age <= 36){
+    console.log("youth");
+}
+
+if(age > 36){
+    console.log("elder")
+}
