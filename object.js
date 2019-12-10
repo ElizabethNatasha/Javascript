b146d457d76f71ceae5446ab042fd78191b386f2
ElizathNatasha <mulekaliz@gmail.com>
2019-12-10 21:46:49 +0300

    removed comments

--- object.js
+++ object.js
@@ -1,10 +1,9 @@
 "use strict"
 let box = {}
 for (let nums = 1; nums< 15; nums++) {
-  // Runs 5 times, with values of step 0 through 4.
-  
+
   box[nums] = nums*nums;
- 
+  
  }
   console.log(box);
 
